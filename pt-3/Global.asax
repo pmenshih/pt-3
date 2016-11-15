3117fd054a97851018fb2a560098c6a199eb56c8
pmenshih <p.menshih@gmail.com>
2016-11-15 14:55:52 +0300

    Add project files.

---
+++ pt-3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="pt_3.MvcApplication" Language="C#" %>
---
+++ pt-3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pt_3.Startup))]
+namespace pt_3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
